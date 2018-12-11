0e001e840b2c9867f86d1f2bdb88b08b1ded6c6d
Eden Vicary <edenvicary@gmail.com>
2018-12-11 15:44:45 -0800

    Setup routes
    This enables multiple pages and opens up navigation.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Redirect, Route, Switch } from 'react-router';
+import { Router } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import Login from './features/Login';
+import Home from './features/Home';
+
+const customHistory = createBrowserHistory();
+
+const Root = () => {
+  return (
+    <Router history={customHistory}>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/app/home" component={Home} />
+        <Redirect from="/" to="/login" />
+      </Switch>
+    </Router>
+  );
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
+serviceWorker.register();
