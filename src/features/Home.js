df35210a34630d69c152cd1eb6dabf91e5ec6329
Eden Vicary <edenvicary@gmail.com>
2018-12-12 16:48:41 -0800

    Add upload a photo
    This enables uploading a photo to firestore which is the first step in
    processing it and all of its companions.
    
    I was using a naming convention of all images but it seems the tutorial
    has some weird differentiation between an image and a photo so I've
    moved it all inline with the naming used in the tutorial to keep it
    logical.

--- src/features/Home.js
+++ src/features/Home.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import { Col, Grid, Image, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { logout } from '../helpers/auth';
+import FileUploader from 'react-firebase-file-uploader';
+import firebase from 'firebase';
 
 const appTokenKey = "appToken";
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
-    const images = [
+    const allPhotos = [
       {
         id: 'placeholder_1',
         url: 'http://fillmurray.com/200/200'
@@ -24,7 +26,7 @@ export default class Home extends Component {
     ];
     this.handleLogout = this.handleLogout.bind(this);
     this.state = {
-      images
+      allPhotos
     };
   }
 
@@ -36,13 +38,51 @@ export default class Home extends Component {
     });
   }
 
+  async handleUploadSuccess(filename) {
+    try {
+      let {
+        bucket,
+        fullPath
+      } = await firebase
+        .storage()
+        .ref('images')
+        .child(filename)
+        .getMetadata();
+      console.log('bucket', bucket);
+      console.log('fullPath', fullPath);
+      let downloadURL = await firebase
+        .storage()
+        .ref('images')
+        .child(filename)
+        .getDownloadURL();
+      console.log('downloadURL', downloadURL);
+      let {
+        uid,
+        email,
+        displayName
+      } = await firebase.auth().currentUser;
+      let newPhoto = {
+        url: downloadURL,
+        userName: displayName,
+        userId: uid,
+        email,
+        bucket,
+        fullPath
+      }
+      console.log('newPhoto', newPhoto);
+      await firebase.firestore().collection('photos').add(newPhoto);
+    } catch(error) {
+      console.error(error);
+    }
+  }
+
   render() {
-    const images = this.state.images.map(image => {
+    const allImages = this.state.allPhotos.map(photo => {
       return (
-        <div key={image.id}>
+        <div key={photo.id}>
           <div style={{minHeight: '215px'}}>
             <i className="bottom-icon material-icons main-close">close</i>
-            <Image style={{ width: '100%' }} src={image.url} responsive />
+            <Image style={{ width: '100%' }} src={photo.url} responsive />
           </div>
         </div>
       );
@@ -50,14 +90,25 @@ export default class Home extends Component {
 
     return (
       <div>
-        {images}
+        {allImages}
         <Grid className="bottom-nav">
           <Row className="show-grid">
             <Col xs={4} className="col-bottom">
               <Link to="/app/album"><i className="bottom-icon material-icons">collections</i></Link>
             </Col>
             <Col xs={4} className="col-bottom">
-              <i className="bottom-icon material-icons">camera_alt</i>
+              <label>
+                <i className="bottom-icon material-icons">camera_alt</i>
+                <FileUploader
+                  hidden
+                  accept="image/*"
+                  storageRef={firebase.storage().ref('images')}
+                  onUploadStart={this.handleUploadStart}
+                  onUploadError={this.hanldeUploadError}
+                  onUploadSuccess={this.handleUploadSuccess}
+                  onProgress={this.handleProgress}
+                />
+              </label>
             </Col>
             <Col onClick={this.handleLogout} xs={4} className="col-bottom">
               <i className="bottom-icon material-icons">assignment_return</i>
