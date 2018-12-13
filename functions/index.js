1933102b4f1ed85145c5b19edd054e7fe1382e24
Eden Vicary <edenvicary@gmail.com>
2018-12-13 10:01:24 -0800

    Add setup for serverless functions
    This enables us to have functions that are processed on the serverside
    in their own little containers.

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,10 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const vision = require('@google-cloud/vision');
+const visionClient = new vision.ImageAnnatotorClient();
+let promise = require('promise');
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
